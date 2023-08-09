8f504087fc53c037fba78beb2facefa376d15da0
Ross Couldrey <roscoesemail@gmail.com>
2023-08-09 11:34:24 -0400

    Adding Dataflows and dashboards and admin switch

--- GetPBIDetails.ps1
+++ GetPBIDetails.ps1
@@ -1,16 +1,31 @@
+########################################
+# This script will get the following details from all workspaces in a Power BI tenant:
+#   - Workspaces
+#   - Reports
+#   - Dashboards
+#   - Datasets
+#
+##########################################
+
 #set variables
 $outputPath = ".\PowerBIDetails\"
 $workspacesFile = "workspaces.csv"
 $reportsFile = "reports.csv"
 $datasetFile = "datasets.csv"
+$dashboardFile = "dashboards.csv"
+$dataflowFile = "dataflows.csv"
 
 $workspaceFilePath = $outputPath + $workspacesFile
 $reportsFilePath = $outputPath + $reportsFile
 $datasetFilePath = $outputPath + $datasetFile
+$dashboardFilePath = $outputPath + $dashboardFile
+$dataflowFilePath = $outputPath + $dataflowFile
 
 #containers for results
 $Reports = @()
 $Datasets = @()
+$dashboards = @()
+$dataflows = @()
 
 #login to powerbi with user account 
 Connect-PowerBIServiceAccount
@@ -30,8 +45,21 @@ Connect-PowerBIServiceAccount
 # # #connect to powerbi
 # Connect-PowerBIServiceAccount -ServicePrincipal -Credential $mycreds -TenantId $tenant
 
+# Get user input for Admin flag
+$Admin = Read-Host -Prompt "Would you like to run this script as a Power BI Admin? (Y/N)"
+
+#if admin then set scope to Organization, else set it to Individual
+if ($Admin -eq "Y"){
+    $scope = "Organization"
+    Write-Host "`r`nRunning scripts using scope 'Organization'.  This will return all workspaces in the tenant and may return errors if you are not using an account with Admin privileges. `r`n"
+}
+else{
+    $scope = "Individual"
+    Write-Host "`r`nRunning scripts using scope 'Individual'.  This will return all workspaces that you have access to. `r`n"
+}
+
 #get workspaces
-$workspaces = Get-PowerBIWorkspace -Scope Organization -All
+$workspaces = Get-PowerBIWorkspace -Scope $scope -All
 
 #write workspace file
 $workspaces 
@@ -56,7 +84,7 @@ foreach ($ws in $workspaces){
     }
    
     #get reports for each workspace
-    $wsReports = Get-PowerBIReport -Scope Organization -WorkspaceId $workspaceId
+    $wsReports = Get-PowerBIReport -Scope $scope -WorkspaceId $workspaceId
 
     #Loop through all reports in each workspace
     foreach ($rpt in $wsReports){
@@ -72,13 +100,29 @@ foreach ($ws in $workspaces){
 
     }#end foreach report
 
+    #get dashboards for each workspace
+    $wsDashboards = Get-PowerBIDashboard -Scope $scope -WorkspaceId $workspaceId
+
+    #Loop through all reports in each workspace
+    foreach ($dash in $wsDashboards){
+        #store report details in custom object
+        $dashboards += [PSCustomObject] @{
+            "workspace_ID" = $workspaceId 
+            "Dashboard" = $dash.Name
+            "Dashboard_ID" = $dash.Id
+            "embedUrl" = $dash.EmbedUrl      
+        } #end PsCustomObject
+
+    }#end foreach dashboard
+
     #get datasets for each workspace
-    $wsDatasets = Get-PowerBIDataset -Scope Organization -WorkspaceId $workspaceId
+    $wsDatasets = Get-PowerBIDataset -Scope $scope -WorkspaceId $workspaceId
 
     #loop through all datasets in each workspace
     foreach ($ds in $wsDatasets){
 
         $datasets += [PSCustomObject]@{
+            "workspace_ID" = $workspaceId
             "DatasetID" = $ds.Id
             "DatasetName" = $ds.Name
             "ConfiguredBy" = $ds.ConfiguredBy
@@ -96,6 +140,23 @@ foreach ($ws in $workspaces){
             "TargetStorageMode" = $ds.TargetStorageMode
         }#end pscustomobject
     }#end foreach dataset
+
+ #get dataflows for each workspace
+ $wsDataFlows = Get-PowerBIDataset -Scope $scope -WorkspaceId $workspaceId
+
+ #loop through all datasets in each workspace
+ foreach ($df in $wsDataFlows){
+
+     $dataflows += [PSCustomObject]@{
+        "Workspace_ID" = $workspaceId 
+        "Dataflow_ID" = $df.Id
+        "DataflowName" = $df.Name
+        "Description"= $df.Description
+        "ModelURL" = $df.ModelURL
+        "ConfiguredBy" = $df.ConfiguredBy
+     }#end pscustomobject
+ }#end foreach dataflow
+
 }#end foreach workspace
 
 ##########################
@@ -107,7 +168,16 @@ $Reports
     | Select-Object -Property workspace_ID, report, report_ID, WebUrl, EmbedUrl, Dataset_ID
     | Export-Csv -Path $reportsFilePath -NoTypeInformation
 
+#write Dashboards file
+$dashboards
+| Select-Object -Property workspace_ID, Dashboard, Dashboard_ID, EmbedUrl
+| Export-Csv -Path $dashboardFilePath -NoTypeInformation
+
 #write datasets file
 $datasets
     | Select-Object -Property DatasetID, DatasetName, ConfiguredBy, DefaultRetentionPolicy, AddRowsApiEnabled, Tables, WebUrl, Relationships, Datasources, DefaultMode,IsRefreshable, IsEffectiveIdentityRequired, IsEffectiveIdentityRolesRequired, IsOnPremGatewayRequired, TargetStorageMode
     | Export-Csv -Path $datasetFilePath -NoTypeInformation
+
+$dataflows
+    | Select-Object -Property Workspace_ID, Dataflow_ID, DataflowName, Description, ModelURL, ConfiguredBy
+    | Export-Csv -Path $dataflowFilePath -NoTypeInformation
